970bb1e3cb96985dfa166cec51e5a4474a6561e0
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:49:52 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+a=10
+b=5
+sum=a+b
+division=a/b
+print ("sum:",sum,"division:",division)
+print("is a greater than b?:",a>b)
+print("is a and b are equal?:",a==b)
+print("logical AND:",a>b and b>a)
+print("logical OR:",a>b and b>a)
+
+
