79484d448961173fc9b0eebc8a071dada26e23e6
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-05 15:50:10 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,5 @@
+import math
+num=int(input("enter a number"))
+print("square root of:",num,"=", math .sqrt(num))
+print("factorial of:",num,"=", math.factorial(num))
+print("raised to number:",num,"=", math.pow(num,2))
