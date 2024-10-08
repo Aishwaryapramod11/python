29e8346814ace55565a342ec275a134d09544c7e
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-08 14:58:41 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+'''
+author: Aishwarya
+date: 08-10-2024
+python program : arithmetic operators.
+version: 1.0
+'''
+num1=int(input("enter your first number"))
+num2=int(input("enter your second number"))
+num3=int(input("enter your third number"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+sub=num2-num1
+print("The difference when num2 is subtracted from num1 is:",sub)
+multiply=num1*num2
+print("The product of num1 and num2 is:",multiply)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is:",division)
+remainder=num1%num2
+print("The remainder when num1 is divided by num2 is:",remainder)
+result = (num1 + num2) * num3 / 2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
+
