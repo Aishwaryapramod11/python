ded8fa1d48a3ee34c518b62c018f6bf4acb97576
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-15 16:11:57 +0530

    Add files via upload

---
+++ no odd.py
@@ -0,0 +1,8 @@
+'''author:Aishwarya
+date:15-10-24
+python code for generating odd no till 10'''
+limit=int(input("enter the limit"))
+odd_number=1
+while odd_number<=limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
