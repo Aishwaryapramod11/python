d156670cf4b44cb703dd89f094e986147f3d0ce2
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-15 15:13:56 +0530

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,10 @@
+'''
+author:Aishwarya
+date:15-10-24
+python code for checking divisibility
+'''
+num=int(input("enter your number"))
+if num%5==0:
+    print("divisible by 5")
+else:
+    print("not divisble by 5")
