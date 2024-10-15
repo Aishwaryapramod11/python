d4cd4141dff77dffa537e90bd350620d8d183c7c
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-15 15:16:14 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,11 @@
+'''
+author:Aishwarya
+date:15-10-24
+python program for checking a number greater than 100
+'''
+num=int(input("enter a number "))
+if num>100:
+    print("number is greater than 100")
+else:
+    print("number",num,"is" " " "less than 100")
+
