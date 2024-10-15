dcc7e04286399050b98b397ba72ef97337b88214
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-15 15:14:54 +0530

    Add files via upload

---
+++ driving.py
@@ -0,0 +1,11 @@
+'''
+author:Aishwarya
+date:15-10-24
+python file for checking eligibilty for driving license
+'''
+age=int(input("enter your age"))
+print(age)
+if age>=16:
+    print("eligible for a driving license")
+else:
+    print("not eligible for a driving license")
