e70177c3c15f7be32a746764303dac26173492a0
Aishwaryapramod11 <aishwaryapramodnair2028@cs.sjcetpalai.ac.in>
2024-10-29 14:36:08 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,13 @@
+'''
+author:Aishwarya
+date:29-10-24
+Python program to check whether a number prime number or not
+'''
+check_prime=int(input("enter a number"))
+for  i in range (2,(check_prime//2)+1):
+   if check_prime % i==0:
+        break
+if i==(check_prime//2):
+     print(f"the given number {""} {check_prime} is prime")
+else:
+       print(f"the given number{""} {check_prime}is not prime")
